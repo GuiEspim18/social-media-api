3b3aac5bfb9b1f689318b35a4ee93cb8a09beb90
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-02-14 21:46:17 -0300

    users constructor done and add active user

--- src/main/java/com/api/model/users/Users.java
+++ src/main/java/com/api/model/users/Users.java
@@ -17,16 +17,18 @@ public class Users {
     public String username;
     public String email;
     private String password;
-    public boolean online = false;
+    public boolean online;
     public Date createdAt;
+    public boolean active;
 
     public Users(UsersDTO data) {
         this.username = data.username();
         this.email = data.email();
         this.password = hashPassword(data.password());
-
+        this.active = true;
+        this.online = false;
+        this.createdAt = new Date();
     }
-
 
     private String hashPassword(String password) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
