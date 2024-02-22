18f30c6f217485f33979e9cb6549deb7c41374b8
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-02-22 08:01:08 -0300

    Likes entity with long

--- src/main/java/com/api/controller/likes/Likes.java
+++ src/main/java/com/api/controller/likes/Likes.java
@@ -1,4 +1,12 @@
 package com.api.controller.likes;
 
+import jakarta.persistence.*;
+
+@Entity(name = "Likes")
+@Table(name = "likes")
 public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
 }
